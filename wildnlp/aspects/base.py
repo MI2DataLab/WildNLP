19d6b13250e68bc99580b0fcbc6189c9ed09f174
adam <adam.slucki@gmail.com>
2019-03-28 22:08:23 +0100

    Chaning __call__ signature

--- wildnlp/aspects/base.py
+++ wildnlp/aspects/base.py
@@ -7,7 +7,7 @@ class Aspect(ABC):
     must implement the __call__ method.
     """
 
-    def __call__(self, sentence, *args, **kwargs):
+    def __call__(self, sentence):
         """We want to directly call
         objects of the Aspect class for easy chaining.
         This function will be applied to sentences.
