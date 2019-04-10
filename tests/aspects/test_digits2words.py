71b73ed5a1a01f41d1608e881259d2ce4716f37a
adam <adam.slucki@gmail.com>
2019-04-10 21:44:25 +0200

    Correcting tests and adding docs

--- tests/aspects/test_digits2words.py
+++ tests/aspects/test_digits2words.py
@@ -5,21 +5,14 @@ def test_basic():
     sentence = '22'
     modified = Digits2Words()(sentence)
 
-    print(modified)
     assert modified == 'twenty-two'
 
     sentence = 'That\'s 23.3.'
     modified = Digits2Words()(sentence)
-
-    print(modified)
 
     assert modified == 'That\'s twenty-three point three.'
 
     sentence = 'Famous 12. 2 to midnight.'
     modified = Digits2Words()(sentence)
 
-    print(modified)
-
     assert modified == 'Famous twelve. two to midnight.'
-
-test_basic()
--- tests/aspects/test_white_spaces.py
+++ tests/aspects/test_white_spaces.py
@@ -4,16 +4,11 @@ from wildnlp.aspects import WhiteSpaces
 def test_basic():
     sentence = 'Test sentence.'
     modified = WhiteSpaces(white_spaces=" \t", add_percentage=100, remove_percentage=0, seed=40)(sentence)
-    print(modified)
+
     assert len(modified) > len(sentence)
 
     sentence = 'Test sentence.'
     modified = WhiteSpaces(white_spaces=" \t", add_percentage=0, remove_percentage=100)(sentence)
-    print(modified)
 
     assert modified[-1] == '.'
     assert len(modified.split()) < len(sentence.split())
-
-
-
-
