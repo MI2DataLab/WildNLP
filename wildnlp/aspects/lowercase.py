95accbfa52cedf855eecc642823ca836cd4823ac
Mayank Lunayach <speedspeeder1@gmail.com>
2019-07-18 16:12:40 +0800

    Simplifying the code

--- wildnlp/aspects/lowercase.py
+++ wildnlp/aspects/lowercase.py
@@ -1,45 +1,18 @@
-import random
-
 from .base import Aspect
 
 
 class LowerCase(Aspect):
-    """Randomly lowercases the dataset
+    """Lower-cases the dataset
 
-    .. caution:: Uses random numbers, default seed is 42.
     """
 
-    def __init__(self, words_percentage=50, seed=42):
-        """
-
-        :param words_percentage: Percentage of the words in the dataset which needs to be lowercased.
-                                 Defaults to 0.5.
-
-        :param seed: Random seed.
-        """
-
-        if words_percentage >= 1:
-            words_percentage /= 100.
-
-        random.seed(seed)
-
     def __call__(self, sentence):
-        tokens = self._tokenize(sentence)
-        lowercased_tokens = self._lowercase_tokens(tokens)
-        return self._detokenize(lowercased_tokens)
+        return " ".join([self._lowercase_word(word)
+                         if word != '' else ''
+                         for word in sentence.split(' ')])
 
     @staticmethod
-    def _lowercase_tokens(tokens):
-
-        modified = []
-        for token in tokens:
-            token = token.lower()
-            modified.append(token)
-        return modified
-
-    @staticmethod
-    def _capitalize(token):
-        try:
-            return token[0].upper() + token[1:]
-        except IndexError:
-            return token
+    def _lowercase_word(word):
+        if len(word) == 0:
+            raise ValueError("Can't lowercase empty words")
+        return word.lower()
